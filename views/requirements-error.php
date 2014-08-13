ccaf8bdcd9e73f446bd5ea11dc1448a63e7b4434
Charlie Merland <charlie.merland@gmail.com>
2014-08-14 00:21:58 +0200

    Better requirements notice

--- views/requirements-error.php
+++ views/requirements-error.php
@@ -18,11 +18,13 @@
 		</li>
 		<li>
 			<strong>WPMovieLibrary <?php echo WPMLTR_REQUIRED_WPML_VERSION; ?>+</strong>
-			<em><?php printf( __( '(You\'re running version %s)', 'wpml-trailers' ), WPML_VERSION ); ?></em>
+			<em><?php if ( is_wpml_active() ) printf( __( '(You\'re running version %s)', 'wpml-trailers' ), WPML_VERSION ); ?></em>
 		</li>
 	</ul>
 
 	<p><?php _e( 'If you need to upgrade your version of PHP you can ask your hosting company for assistance, and if you need help upgrading WordPress you can refer to <a href="http://codex.wordpress.org/Upgrading_WordPress">the Codex</a>.', 'wpml-trailers' ); ?></p>
+
+	<p><?php _e( 'If you tried activating WPMovieLibrary-Trailers without activating WPMovieLibrary first, you will need to deactivate and reactivate WPMovieLibrary-Trailers for this notice to disapear. <a href="http://wpmovielibrary.com/wpmovielibrary-trailers/documentation/installation/#requirements">Learn why</a>.', 'wpml-trailers' ); ?></p>
 <?php endif; ?>
 
 </div>

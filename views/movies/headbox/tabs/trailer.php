1a2467d7e2225d0a596bb6d074a6295a07148a4a
Charlie Merland <charlie@caercam.org>
2014-11-01 11:15:23 +0100

    No trailer headbox notice

--- views/movies/headbox/tabs/trailer.php
+++ views/movies/headbox/tabs/trailer.php
@@ -10,4 +10,8 @@
  */
 ?>
 
+<?php if ( '' != $trailer ) : ?>
 				<iframe src="<?php echo $trailer ?>" frameborder="0"></iframe>
+<?php else : ?>
+				<p><?php _e( 'No trailer added for this movie.', 'wpmovielibrary-trailers' ) ?></p>
+<?php endif; ?>
